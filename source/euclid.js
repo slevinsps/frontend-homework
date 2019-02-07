bf99221a13974a8a166a9afae5971c4c6718240c
Ivan <spasenovivan@mail.ru>
2019-02-08 00:40:29 +0300

    delete tabs

--- source/euclid.js
+++ source/euclid.js
@@ -5,34 +5,34 @@ function isInt(n){
 }
 
 function nodTwoNumbers(x, y) {
-	if (!isInt(x) || !isInt(y)) {
-		return;
-	}
-	let res = 1;
-	if (x != 0) {
-		res = nodTwoNumbers(y % x, x);
-	} else { 
-		res = y;
-	}
-	return res;
+    if (!isInt(x) || !isInt(y)) {
+        return;
+    }
+    let res = 1;
+    if (x != 0) {
+        res = nodTwoNumbers(y % x, x);
+    } else { 
+        res = y;
+    }
+    return res;
 }
 
 function euclid(x1, ...numbers) {
-	if (!isInt(x1)) {
-		return;
-	}
-	
-	let res = x1;
-	let length = numbers.length;
-	let checkType = true;
-	for (let i = 0; i < length; i++) {
-		if (!isInt(numbers[i])) {
-			checkType = false;
-			break;
-		}
-		res = nodTwoNumbers(res, numbers[i]);
-	}
-	if (!checkType)
-		return;
-	return res;
+    if (!isInt(x1)) {
+        return;
+    }    
+    
+    let res = x1;
+    let length = numbers.length;
+    let checkType = true;
+    for (let i = 0; i < length; i++) {
+        if (!isInt(numbers[i])) {
+            checkType = false;
+            break;
+        }
+        res = nodTwoNumbers(res, numbers[i]);
+    }
+    if (!checkType)
+        return;
+    return res;
 }
