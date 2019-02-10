84b639272f4bb23e96fcbe9c58034900a0cbc3e3
Ivan <spasenovivan@mail.ru>
2019-02-10 23:41:29 +0300

    add changes

--- source/euclid.js
+++ source/euclid.js
@@ -1,38 +1,34 @@
 'use strict';
 
-function isInt(n){
-    return Number(n) === n && n % 1 === 0;
-}
-
-function nodTwoNumbers(x, y) {
-    if (!isInt(x) || !isInt(y)) {
+var nodTwoNumbers = (x, y) => {
+    if (x !== parseInt(x, 10) || y !== parseInt(y, 10)) {
         return;
     }
     let res = 1;
-    if (x != 0) {
-        res = nodTwoNumbers(y % x, x);
-    } else { 
-        res = y;
-    }
+    res = x != 0 ? nodTwoNumbers(y % x, x) : y;
     return res;
 }
 
-function euclid(x1, ...numbers) {
-    if (!isInt(x1)) {
+var euclid = (x1, ...numbers) => {
+    if (x1 !== parseInt(x1, 10)) {
         return;
     }    
     
     let res = x1;
     let length = numbers.length;
     let checkType = true;
-    for (let i = 0; i < length; i++) {
-        if (!isInt(numbers[i])) {
-            checkType = false;
-            break;
+    var NumberNotIntException = {};
+    try {
+        numbers.forEach(function(item) {
+            if (item !== parseInt(item, 10)) {
+                throw NumberNotIntException;
+            }
+            res = nodTwoNumbers(res, item);
+        });
+    } catch (e) {
+        if (e === NumberNotIntException) {
+            return;        
         }
-        res = nodTwoNumbers(res, numbers[i]);
     }
-    if (!checkType)
-        return;
     return res;
 }
