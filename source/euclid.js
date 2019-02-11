0489464026eef135c61c01f2d110607642a6ed52
Ivan <spasenovivan@mail.ru>
2019-02-11 15:31:00 +0300

    fixed

--- source/euclid.js
+++ source/euclid.js
@@ -1,25 +1,39 @@
 'use strict';
 
-var nodTwoNumbers = (x, y) => {
+/**
+ * NOD of two numbers
+ * 
+ * @param {number} x - first number
+ * @param {number} y - second number 
+ * @returns {(number|undefined)} NOD(x,y).
+ */
+let nodTwoNumbers = (x, y) => {
     if (x !== parseInt(x, 10) || y !== parseInt(y, 10)) {
         return;
     }
     let res = 1;
-    res = x != 0 ? nodTwoNumbers(y % x, x) : y;
+    res = x !== 0 ? nodTwoNumbers(y % x, x) : y;
     return res;
 }
 
-var euclid = (x1, ...numbers) => {
+
+/**
+ * NOD of any numbers
+ * 
+ * @param {number} x1 - first number
+ * @param {Array} numbers - other numbers 
+ * @returns {(number|undefined)} NOD of any numbers.
+ */
+let euclid = (x1, ...numbers) => {
     if (x1 !== parseInt(x1, 10)) {
         return;
     }    
     
     let res = x1;
-    let length = numbers.length;
     let checkType = true;
-    var NumberNotIntException = {};
+    let NumberNotIntException = {};
     try {
-        numbers.forEach(function(item) {
+        numbers.forEach( item => {
             if (item !== parseInt(item, 10)) {
                 throw NumberNotIntException;
             }
